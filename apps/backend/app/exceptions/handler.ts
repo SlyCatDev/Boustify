78d8f580a6e3bc86209d3f7fb31cae08ab9ba816
SylvainDev <sylvain.raveneau1@gmail.com>
2025-09-19 16:21:59 +0200

    [⚙️coding] backend - handler : gestion de l'erreur E_INVALID_CREDENTIALS

--- apps/backend/app/exceptions/handler.ts
+++ apps/backend/app/exceptions/handler.ts
@@ -1,4 +1,5 @@
 import app from '@adonisjs/core/services/app'
+import { errors } from '@adonisjs/auth'
 import { HttpContext, ExceptionHandler } from '@adonisjs/core/http'
 
 export default class HttpExceptionHandler extends ExceptionHandler {
@@ -7,12 +8,17 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * with pretty printed stack traces.
    */
   protected debug = !app.inProduction
+  protected renderStatusPages = app.inProduction
 
   /**
    * The method is used for handling errors and returning
    * response to the client
    */
   async handle(error: unknown, ctx: HttpContext) {
+    if (error instanceof errors.E_INVALID_CREDENTIALS) {
+      return ctx.response.status(error.status).send(error.getResponseMessage(error, ctx))
+    }
+
     return super.handle(error, ctx)
   }
 
